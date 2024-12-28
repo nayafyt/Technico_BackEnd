8f37308a2883e12c7f485ff431bb0c62954f584e
NayaF <nfytali@gmail.com>
2024-12-28 22:16:47 +0200

    Add PropertOwner Model

---
+++ TechnicoApp/Models/PropertyOwner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicoApp.Models;
+
+public class PropertyOwner
+{  
+    public string VatNumber { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public UserType UserType { get; set; }
+}
---
+++ TechnicoApp/Models/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicoApp.Models;
+
+public enum UserType
+{
+    User,Admin
+}
