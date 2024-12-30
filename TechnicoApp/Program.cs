fa3788697a2de9297c4fb9edd1ef77dca4d2bf95
NayaF <nfytali@gmail.com>
2024-12-30 17:32:58 +0200

    Merge back-end files with implementatios to correct

--- TechnicoApp/Program.cs
+++ TechnicoApp/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("hello");
+﻿Console.WriteLine($"""
+        Technico -  AccentureXCodeHub Project
+        Christina Galani
+        Maria Moutousi 
+        Angelos Osf
+        Naya Fytali
+        """);
